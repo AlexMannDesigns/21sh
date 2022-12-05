984cae98b04c5bf595322fc4d4b56d89b5b7b7af
Alexander Mann <amann@student.hive.fi>
2022-12-05 19:07:12 +0200

    added basic functionality to create the internal vars array in the state struct and add new variables to it

--- src/execution/internal_variables.c
+++ src/execution/internal_variables.c
@@ -6,19 +6,29 @@
 /*   By: amann <amann@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 18:23:18 by amann             #+#    #+#             */
-/*   Updated: 2022/12/05 18:33:54 by amann            ###   ########.fr       */
+/*   Updated: 2022/12/05 18:59:05 by amann            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "execution.h"
 
+//TODO error handling, esp with mallocs
+
 void	set_internal_variables(char **var_list, t_state *state)
 {
-	(void) state;
-
 	size_t	i;
+	size_t	j;
 
+	if (!(state->intern))
+		state->intern = (char **) ft_memalloc(sizeof(char *) * (INPUT_MAX_SIZE / 2));
+	j = ft_null_array_len((void **) state->intern);
 	i = 0;
 	while (var_list[i])
-		ft_putendl(var_list[i++]);
+	{
+		(state->intern)[j] = ft_strdup(var_list[i]);
+		j++;
+		i++;
+	}
+	for (i = 0 ; state->intern[i]; i++)
+		ft_putendl(state->intern[i]);
 }
